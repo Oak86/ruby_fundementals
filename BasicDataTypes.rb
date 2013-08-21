4919d2b7162abe13138e47735ac337e193bd97cc
oak86 <email@omarkhafagy.com>
2013-08-20 21:17:41 -0400

    Add comments for future exercises so I can skip ahead and do what I understand first.

--- BasicDataTypes.rb
+++ BasicDataTypes.rb
@@ -1,3 +1,4 @@
+#---Numbers---
 # Max
 arr.sort.last
 	#Didn't realize there was a .max method. Will use next time.
@@ -10,3 +11,22 @@ fibo = [0,1,1,2,3,5,8,13,21,34,55]
 return fibo[n]
 
 # 
+
+##---Strings---##
+
+#Reverse
+
+#Hello World
+
+#There's No Way This Works
+
+#There's No Way This Works Version 2
+
+#Brackets and Searches
+
+#No Limit
+
+##---Operators---##
+
+# && Versus And
+# Hiring Programmers -Boolean Expressions in Ruby
